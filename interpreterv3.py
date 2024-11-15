cd0bdb70a29806d1061a0576a846b0f102f7b5c6
Jarod Houston <jarodhouston26@gmail.com>
2024-11-14 23:14:39 -0800

    progress made

--- interpreterv3.py
+++ interpreterv3.py
@@ -7,8 +7,34 @@ class Type:
     INT = "int"
     BOOL = "bool"
     STRING = "string"
+    NODE = "node"
     NIL = "nil"
 
+class Struct:
+    def __init__(self, name, attr, alive):
+        self.name = name
+        self.alive = alive
+        self.type = name
+        attr_list = {}
+        for elem in attr:
+            default_val = None
+            type = elem.get("var_type")
+            if type == "bool":
+                default_val = False
+            if type == "int":
+                default_val = 0
+            if type == "string":
+                default_val = ""
+            attr_list[elem.get("name")] = Value(type, default_val)
+        self.attr = attr_list
+    
+    def set_alive(self, state):
+        self.alive = state
+    
+    def print(self):
+        print(self.name)
+        print(self.attr)
+        
 
 # Represents a value, which has a type and its value
 class Value:
@@ -23,7 +49,13 @@ class Value:
         return self.type
     
     def change_value(self, val):
-        self.value = val
+        if self.type == Type.BOOL and isinstance(val, int):
+            if val == 0:
+                self.value = False
+            else:
+                self.value = True
+        else:
+            self.value = val
 
 class Interpreter(InterpreterBase):
     def __init__(self, console_output=True, inp=None, trace_output=False):
@@ -38,8 +70,14 @@ class Interpreter(InterpreterBase):
         ast = parse_program(program)
         # Stack of stacks of dictionaries
         self.stack = []
+        # List of Struct objects
+        self.structs = []
 
         self.func_list = self.get_func_list(ast)
+        # List of struct objects
+        self.struct_list = []
+        # List of defined structs
+        self.defined_structs = self.get_defined_structs(ast)
         main_func_node = next((value for key, value in self.func_list.items() if key[0] == "main"), None)
         if not main_func_node:
             super().error(
@@ -54,6 +92,19 @@ class Interpreter(InterpreterBase):
         for function in ast.get("functions"):
             func_list[(function.get("name"), len(function.get("args")))] = function
         return func_list
+    
+    def get_defined_structs(self, ast):
+        struct_list = []
+        for struct in ast.get("structs"):
+            struct_list.append({struct.get("name"): struct.get("fields")})
+            # struct_list.append(Struct(struct.get("name"), struct.get("fields"), False))
+        return struct_list
+    
+    def get_struct_fields(self, name):
+        for struct in self.defined_structs:
+            if name in struct:
+                return struct[name]
+        return None
 
     def run_func(self, func_node, call_arguments):
         list_to_append = [{}]
@@ -61,7 +112,16 @@ class Interpreter(InterpreterBase):
         for i in range(len(func_args)):
             var_name = func_args[i].get("name")
             var_type = func_args[i].get("var_type")
-            list_to_append[0][var_name] = Value(var_type, self.evaluate_expression(call_arguments[i]))
+            val = self.evaluate_expression(call_arguments[i])
+            if self.is_valid_return(val, var_type):
+                if var_type == "bool" and self.is_int(val):
+                    val = (val != 0)
+                list_to_append[0][var_name] = Value(var_type, val)
+            else: 
+                super().error(
+                    ErrorType.TYPE_ERROR,
+                    "Incompatible types for function's arguments",
+                )
 
         self.stack.append(list_to_append)
         self.stack[-1].append({})
@@ -82,6 +142,7 @@ class Interpreter(InterpreterBase):
                 self.do_definition(statement)
             elif statement.elem_type == "=":
                 self.do_assignment(statement)
+                # print(self.stack[-1][-1]['p'].value.print())
             elif statement.elem_type == "fcall":
                 result = self.do_func_call(statement)              
             elif statement.elem_type == "if":
@@ -103,6 +164,9 @@ class Interpreter(InterpreterBase):
             return Value(Type.BOOL, False)
         if type == "string":
             return Value(Type.STRING, "")
+        for struct in self.defined_structs:
+            if type in struct:
+                return Value(Type.NODE, Type.NIL)
         else:
             raise ValueError("Unknown value type")
     
@@ -116,22 +180,65 @@ class Interpreter(InterpreterBase):
             )
         else:
             self.stack[-1][-1][var_name] = self.create_value(var_type)
-            # self.stack[-1][-1][var_name] = None
+    
+    def is_valid_assignment(self, curr_var, val):
+        if (type(val) == bool or type(val) == int) and curr_var.type == Type.BOOL:
+            return True
+        elif type(val) == int and curr_var.type == Type.INT:
+            return True
+        elif type(val) == str and curr_var.type == Type.STRING:
+            return True
+        elif (type(val) == Struct or val == Type.NIL) and (curr_var.type == Type.NODE or self.struct_exists(curr_var.type)):
+            return True
+        return False
+    
+    def get_var_name_split(self, var_name):
+        struct_name = None
+        attr_name = None
+        if "." in var_name:
+            struct_name, attr_name = var_name.split('.')
+        if struct_name and attr_name:
+            return (struct_name, attr_name)
+        return (var_name,)
     
     def do_assignment(self, assignment):
         var_name = assignment.get("name")
+        split = self.get_var_name_split(var_name)
+        if len(split) > 1:
+            var_name = split[0]
         scope = self.get_scope(var_name)
         if scope:
-        # if var_name not in self.stack[-1][self.curr_func]:
-        #     super().error(
-        #         ErrorType.NAME_ERROR,
-        #         f"Variable {var_name} has not been defined",
-        #     )
-        # else:
             expression = assignment.get("expression")
             expression_result = self.evaluate_expression(expression)
-            scope[var_name].change_value(expression_result)
-            # scope[var_name] = expression_result
+            if len(split) > 1:
+                if self.is_valid_assignment(scope[var_name].value.attr[split[1]], expression_result):
+                    scope[var_name].value.attr[split[1]].change_value(expression_result)
+            elif self.is_valid_assignment(scope[var_name], expression_result):
+                scope[var_name].change_value(expression_result)
+            else:
+                super().error(
+                    ErrorType.TYPE_ERROR,
+                    "Incompatible types for assignment",
+                )
+        # if len(split) > 1:
+        #     print(scope[var_name].value.attr[split[1]].value)
+            
+    def evaluate_coercion(self, val1, val2):
+        if val1 == 0:
+            return not val2
+        return val2
+    
+    def is_int(self, val):
+        if isinstance(val, int) and not isinstance(val, bool):
+            return True
+        return False
+    
+    def evaluate_equality(self, val1, val2):
+        if self.is_int(val1) and isinstance(val2, bool):
+            return self.evaluate_coercion(val1, val2)
+        if self.is_int(val2) and isinstance(val1, bool):
+            return self.evaluate_coercion(val2, val1)
+        return val1 == val2
 
     def evaluate_expression(self, expression):
         exp_type = expression.elem_type
@@ -149,17 +256,36 @@ class Interpreter(InterpreterBase):
                     ErrorType.TYPE_ERROR,
                     "Incompatible types for arithmetic operation",
                 )
+            
+            op_map = {
+                "&&": lambda x, y: (x != 0 if self.is_int(x) else x) and (y != 0 if self.is_int(y) else y),
+                "||": lambda x, y: (x != 0 if self.is_int(x) else x) or (y != 0 if self.is_int(y) else y)
+            }
 
-            if exp_type == "&&": return val1 and val2
-            if exp_type == "||": return val1 or val2
+            if exp_type in op_map:
+                return op_map[exp_type](val1, val2)
 
-            if exp_type == "==" and not isinstance(val1, type(val2)):
-                return False
-            if exp_type == "!=" and not isinstance(val1, type(val2)):
-                return True
+            # if exp_type == "&&": 
+            #     if self.is_int(val1) and isinstance(val2, bool):
+            #         return val1 != 0 and val2
+            #     if self.is_int(val2) and isinstance(val1, bool):
+            #         return val2 != 0 and val1
+            #     return val1 and val2
+            # if exp_type == "||":
+            #     if self.is_int(val1) and isinstance(val2, bool):
+            #         return val1 != 0 or val2
+            #     if self.is_int(val2) and isinstance(val1, bool):
+            #         return val2 != 0 or val1
+            #     return val1 or val2
 
-            if exp_type == "==": return val1 == val2
-            if exp_type == "!=": return val1 != val2
+            # if exp_type == "==" and not isinstance(val1, type(val2)):
+            # if exp_type == "==":
+            #     return False
+            # if exp_type == "!=" and not isinstance(val1, type(val2)):
+            #     return True
+
+            if exp_type == "==": return self.evaluate_equality(val1, val2)
+            if exp_type == "!=": return not self.evaluate_equality(val1, val2)
             if exp_type == ">": return val1 > val2
             if exp_type == "<": return val1 < val2
             if exp_type == ">=": return val1 >= val2
@@ -170,7 +296,10 @@ class Interpreter(InterpreterBase):
             if exp_type == "*": return val1 * val2
             if exp_type == "/": return int(val1 // val2)
 
-            return "Error in evaluating expression"
+            super().error(
+                ErrorType.TYPE_ERROR,
+                "Error in evaluating expression",
+            )
         
         if exp_type in self.unary_ops:
             op1 = expression.get("op1")
@@ -183,20 +312,34 @@ class Interpreter(InterpreterBase):
                 )
             
             if exp_type == "neg": return val1 * -1
-            if val1 is False: return True
-            return False
+            if exp_type == "!":
+                if not isinstance(val1, bool) and isinstance(val1, int):
+                    if val1 == 0:
+                        return True
+                    return False
+                if val1 is False: return True
+
+            super().error(
+                ErrorType.TYPE_ERROR,
+                "Error in evaluating expression",
+            )
         
         # Handles evaluating assignment to value
         if exp_type == "int" or exp_type == "string" or exp_type == "bool":
             return expression.get("val")
         
         if exp_type == "nil":
-            return None
+            return Type.NIL
         
         # Handles evaluating assignment to variable
         if exp_type == "var":
-            value = self.get_variable(expression.get("name"))
+            value = self.get_variable(expression.get("name")).value
             return value
+        
+        if exp_type == "new":
+            struct_type = expression.get("var_type")
+            fields = self.get_struct_fields(struct_type)
+            return Struct(struct_type, fields, False)
 
         if exp_type == "fcall":
             if expression.get("name") == "inputi" or expression.get("name") == "inputs":
@@ -219,7 +362,10 @@ class Interpreter(InterpreterBase):
                     total_output = total_output + str(self.do_func_call(arg))
                 else:
                     if arg.elem_type == "var":
-                        value = self.get_variable(arg.get("name"))
+                        value = self.get_variable(arg.get("name")).value
+                        if type(value) is Struct:
+                            split = self.get_var_name_split(arg.get("name"))
+                            value = value.attr[split[1]].value
                     elif arg.elem_type in self.bin_ops or arg.elem_type in self.bin_bool_ops or arg.elem_type in self.unary_ops or arg.elem_type in self.comp_ops:
                         value = self.evaluate_expression(arg)
 
@@ -238,7 +384,16 @@ class Interpreter(InterpreterBase):
                     ErrorType.NAME_ERROR,
                     f"Function {func_name} has not been defined",
                 )
-            return self.run_func(func_node, func_call.get("args"))
+            result = self.run_func(func_node, func_call.get("args"))
+            return_type = func_node.get("return_type")
+            if result is None:
+                result = self.get_default_value(return_type)
+            if self.is_valid_return(result, return_type):
+                return result
+            super().error(
+                ErrorType.TYPE_ERROR,
+                f"Returned value does not match function's return type"
+            )
     
     def do_if_statement(self, if_statement):
         statements = if_statement.get("statements")
@@ -246,7 +401,12 @@ class Interpreter(InterpreterBase):
         self.stack[-1].append({})
 
         condition = self.evaluate_expression(if_statement.get("condition"))
-        if not isinstance(condition, bool):
+        if not isinstance(condition, bool) and isinstance(condition, int):
+            if condition == 0:
+                condition = False
+            else:
+                condition = True
+        elif not isinstance(condition, bool):
             super().error(
                 ErrorType.TYPE_ERROR,
                 f"If statement condition does not evaluate to boolean"
@@ -269,15 +429,16 @@ class Interpreter(InterpreterBase):
 
         self.do_assignment(init)
 
-        if not isinstance(self.evaluate_expression(condition), bool):
+        if not isinstance(self.evaluate_expression(condition), bool) and not isinstance(self.evaluate_expression(condition), int):
             super().error(
                 ErrorType.TYPE_ERROR,
                 f"For loop condition does not evaluate to boolean",
             )
+
+        result = None
         
         while self.evaluate_expression(condition):
             self.stack[-1].append({})
-            result = None
             # for s in statements:
             stack_length = len(self.stack)
             result = self.run_statements(statements)
@@ -296,8 +457,10 @@ class Interpreter(InterpreterBase):
     def get_exp_value(self, op):
         op_type = op.elem_type
 
+        if op_type == Type.NIL:
+            return op_type
         if op_type == "var":
-            val = self.get_variable(op.get("name"))
+            val = self.get_variable(op.get("name")).value
         elif op_type in self.bin_ops or op_type in self.bin_bool_ops or op_type in self.unary_ops or op_type in self.comp_ops:
             val = self.evaluate_expression(op)
         elif op_type == "fcall":
@@ -320,9 +483,18 @@ class Interpreter(InterpreterBase):
         )
     
     def get_variable(self, var_name):
+        attribute = None
+        if "." in var_name:
+            var_name, attribute = var_name.split('.')
         scope = self.get_scope(var_name)
+        if attribute:
+            return scope[var_name].value.attr[attribute]
         if scope:
-            return scope[var_name].value
+            return scope[var_name]
+        super().error(
+            ErrorType.NAME_ERROR,
+            f"Variable {var_name} has not been defined",
+        )
     
     def handle_input(self, func_name, args):
         if len(args) > 1:
@@ -339,12 +511,24 @@ class Interpreter(InterpreterBase):
             return user_input
     
     def is_not_type_valid_op(self, exp_type, val1, val2):
+        print(val1)
+        print(val2)
         if exp_type in self.comp_ops:
-            if not exp_type == "==" and not exp_type == "!=":
-                if isinstance(val1, bool): return True
-                if not isinstance(val1, int): return True
-            else:
+            if exp_type == "==" or exp_type == "!=":
+                if isinstance(val1, int) and isinstance(val2, int):
+                    return False
+                if val1 == "nil" or val2 == "nil":
+                    return False
+        # if exp_type in self.comp_ops:
+        #     if not exp_type == "==" and not exp_type == "!=":
+        #         if isinstance(val1, bool): return True
+        #         if not isinstance(val1, int): return True
+        #     else:
+        #         return False
+        if exp_type in self.bin_bool_ops:
+            if isinstance(val1, int) and isinstance(val2, int):
                 return False
+            if not isinstance(val1, bool): return True
         if type(val1) != type(val2): return True
         if exp_type in self.bin_ops:
             if isinstance(val1, bool): return True
@@ -352,12 +536,32 @@ class Interpreter(InterpreterBase):
                 if not isinstance(val1, (int, str)): return True
             else:
                 if not isinstance(val1, int): return True
-        if exp_type in self.bin_bool_ops:
-            if not isinstance(val1, bool): return True
 
     def is_not_type_valid_unary_op(self, exp_type, val1):
         if exp_type == "neg":
             if isinstance(val1, bool): return True
             if not isinstance(val1, int): return True
         if exp_type == "!":
-            if not isinstance(val1, bool): return True
+            if not isinstance(val1, int): return True
+    
+    def is_valid_return(self, result, return_type):
+        if (return_type == "bool" and isinstance(result, int) or
+           (return_type == "int" and not isinstance(result, bool) and isinstance(result, int)) or 
+           (return_type == "string" and isinstance(result, str)) or
+           (self.struct_exists(return_type) and ((result and result != Type.NIL and return_type == result.type) or not result or result == Type.NIL)) or
+           (return_type == "void" and result == None)):
+            return True
+        return False
+
+    def get_default_value(self, type):
+        if type == "bool":
+            return False
+        if type == "int":
+            return 0
+        if type == "string":
+            return ""
+        if(self.struct_exists(type)):
+            return Type.NIL
+    
+    def struct_exists(self, name):
+        return any(name in struct for struct in self.defined_structs)
